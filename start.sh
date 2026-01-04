c46dd19707feadd3270ef511fa0628d951583018
chukwuagoziesolomon <chukwuagoziesolomon@gmail.com>
2026-01-04 17:00:38 +0100

    first commit

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-source /opt/render/project/src/.venv/bin/activate
-python manage.py migrate
-python populate.py
-python reset_passwords.py
-python manage.py runserver 0.0.0.0:$PORT
+python3 manage.py migrate
+python3 populate.py
+python3 reset_passwords.py
+python3 manage.py runserver 0.0.0.0:$PORT

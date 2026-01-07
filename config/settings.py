fffb70d235a5a5cdd61436a29ad7bbba07ea663f
chukwuagoziesolomon <chukwuagoziesolomon@gmail.com>
2026-01-07 11:23:31 +0100

    second commit

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:3000',
+    'https://midwestern-bank-admin.vercel.app',
     'https://your-vercel-app.vercel.app',
     'https://your-live-website.com'
 ]
